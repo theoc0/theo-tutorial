7191b10b6d3114ca7ff0754d2e7318b7dff3976d
theoc0 <91440565+theoc0@users.noreply.github.com>
2026-03-29 14:43:58 +0800

    Refactor QUESTION_BANKS to conditionally include banks

--- question-banks.js
+++ question-banks.js
@@ -1,5 +1,5 @@
-const QUESTION_BANKS = [
-  BANK_CTS3,
-  BANK_JT3,
-  BANK_DSE6
-];
+const QUESTION_BANKS = [];
+
+if (typeof BANK_CTS3 !== "undefined") QUESTION_BANKS.push(BANK_CTS3);
+if (typeof BANK_JT3 !== "undefined") QUESTION_BANKS.push(BANK_JT3);
+if (typeof BANK_DSE6 !== "undefined") QUESTION_BANKS.push(BANK_DSE6);
