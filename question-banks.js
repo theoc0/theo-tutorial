32e484f7e3ef2b2ba23853bc3a249a23e42eb7a4
theoc0 <91440565+theoc0@users.noreply.github.com>
2026-05-28 14:02:08 +0800

    Add BANK_JT_SEM2 to QUESTION_BANKS if defined

--- question-banks.js
+++ question-banks.js
@@ -4,3 +4,4 @@ if (typeof BANK_CTS3 !== "undefined") QUESTION_BANKS.push(BANK_CTS3);
 if (typeof BANK_JT3 !== "undefined") QUESTION_BANKS.push(BANK_JT3);
 if (typeof BANK_DSE6 !== "undefined") QUESTION_BANKS.push(BANK_DSE6);
 if (typeof BANK_CTS_SEM2 !== "undefined") QUESTION_BANKS.push(BANK_CTS_SEM2);
+if (typeof BANK_JT_SEM2 !== "undefined") QUESTION_BANKS.push(BANK_JT_SEM2);
